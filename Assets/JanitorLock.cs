adad834161fa3b58f6d0fed53bb33a2b9eb5cec9
Nep <49031387+MelancholyOfNep@users.noreply.github.com>
2023-04-22 01:47:23 -0400

    Fixed janitor lock

--- Assets/JanitorLock.cs
+++ Assets/JanitorLock.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class JanitorLock : MonoBehaviour
 {
+	[SerializeField]
 	GameObject janitorDoor;
 
 	private void Awake()
 	{
-		if (!InvestigationManager.evidence.ContainsKey("ch2evidence0"))
-			InvestigationManager.evidence.Add("ch2evidence0", 0);
+		if (!InvestigationManager.evidence.ContainsKey("ch2Evidence0"))
+			InvestigationManager.evidence.Add("ch2Evidence0", 0);
 		janitorDoor = GameObject.Find("JanitorDoorCollider");
-		if (InvestigationManager.evidence["ch2evidence0"] != 1)
+		if (InvestigationManager.evidence["ch2Evidence0"] != 1)
 		{
             janitorDoor.GetComponent<ChangeScene>().enabled = false;
 			janitorDoor.GetComponent<MeshCollider>().isTrigger = false;
         }
+		else
+		{
+			janitorDoor.GetComponent<ChangeScene>().enabled = true;
+			janitorDoor.GetComponent<MeshCollider>().isTrigger = true;
+		}
 			
 	}
 
